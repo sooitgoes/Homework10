329a9fae7e3ffcc9bbeeb44987c629cd52cde579
Mikhail Laty <laty.mischa@gmail.com>
2023-05-26 21:38:12 +0300

    Initial Commit

---
+++ Homework10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework10
+//
+//  Created by Михаил Латий on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
